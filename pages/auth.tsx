516653420054264d366ffc0f335765104998fa2b
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-10-13 13:48:17 +0530

    add: google oauth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -98,7 +98,9 @@ const Auth = () => {
             >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
-            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+            <div 
+              onClick={() => signIn("google", { callbackUrl: "/" })}
+            className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                 <FcGoogle size={30} />
               </div>
