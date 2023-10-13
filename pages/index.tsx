325df2a22844b4de26be35bd54cce6a4278f4a17
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-10-13 16:15:51 +0530

    add: user sessions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,33 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if(!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+  
+}
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
     <h1>Netflix Clone</h1>
+    <p className="text-white">Logged in as : {user?.name}</p>
+    <button onClick={() => signOut()} className="h-10 w-full bg-white">Sign Out</button>
     </>
   );
 }
