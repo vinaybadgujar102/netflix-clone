77f765bbe1ad51e6693277f9a843f33deb84f50a
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-10-13 19:53:06 +0530

    add: Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -21,13 +21,10 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
 
   return (
     <>
-    <h1>Netflix Clone</h1>
-    <p className="text-white">Logged in as : {user?.name}</p>
-    <button onClick={() => signOut()} className="h-10 w-full bg-white">Sign Out</button>
+      <Navbar />
     </>
   );
 }
