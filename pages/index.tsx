556950bbe0c9a63a4bddeaa73c09f9c6c7316480
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-11-05 19:45:19 +0530

    add: billboard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
+import Billboard from "@/components/Billboard";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -25,6 +26,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <Billboard />
     </>
   );
 }
