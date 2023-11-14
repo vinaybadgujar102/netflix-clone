359ff58a59bdfee8afec7afc0f2f3a914466a669
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-11-14 11:01:34 +0530

    feat: favorite movie functionality

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -3,15 +3,53 @@ import React, { useCallback, useMemo } from "react";
 
 import useCurrentUser from "../hooks/useCurrentUser";
 import useFavorites from "../hooks/useFavorites";
-import { AiOutlinePlus } from "react-icons/ai";
+import { AiOutlineCheck, AiOutlinePlus } from "react-icons/ai";
 
 interface FavoriteButtonProps {
     movieId: string
 }
 
 const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
+    const { mutate: mutateFavorites } = useFavorites();
+    const { data: currentUser, mutate } = useCurrentUser();
+
+    const isFavorite = useMemo(() => {
+        const list = currentUser?.favoriteIds || [];
+
+        return list.includes(movieId);
+    }, [currentUser, movieId]);
+
+    const toggleFavorites = useCallback(async () => {
+        let response;
+
+        if (isFavorite) {
+            response = await axios.delete('/api/favorite', {
+                data: {
+                    movieId
+                }
+            });
+        } else {
+            response = await axios.post('/api/favorite', {
+                movieId
+            });
+        }
+
+        const updatedFavoriteIds = response?.data?.favoriteIds;
+
+        mutate({
+            ...currentUser,
+            favoriteIds: updatedFavoriteIds
+        });
+
+        mutateFavorites();
+    }, [movieId, isFavorite, currentUser, mutate, mutateFavorites]);
+
+    const Icon = isFavorite ? AiOutlineCheck : AiOutlinePlus;
+
     return (
-        <div className="
+        <div 
+        onClick={toggleFavorites}
+        className="
             cursor-pointer
             group/item
             w-6
@@ -27,7 +65,7 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
             transition
             hover:border-neutral-300
         ">
-            <AiOutlinePlus className="text-white" size={25}/>
+            <Icon className="text-white" size={25}/>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
-import Navbar from "@/components/Navbar";
-import { NextPageContext } from "next";
-import { getSession } from "next-auth/react";
-import Billboard from "@/components/Billboard";
-import MovieList from "@/components/MovieList";
-import useMovieList from "@/hooks/useMovieList";
+import React from 'react';
+import { NextPageContext } from 'next';
+import { getSession } from 'next-auth/react';
+
+import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import useMovieList from '@/hooks/useMovieList';
+import useFavorites from '@/hooks/useFavorites';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
-  if(!session) {
+  if (!session) {
     return {
       redirect: {
-        destination: "/auth",
+        destination: '/auth',
         permanent: false,
       }
     }
@@ -20,19 +23,22 @@ export async function getServerSideProps(context: NextPageContext) {
   return {
     props: {}
   }
-  
 }
 
-export default function Home() {
-  const {data: movies = []} = useMovieList();
+const Home = () => {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
 
   return (
     <>
       <Navbar />
       <Billboard />
       <div className="pb-40">
-        <MovieList title="Trending Now" data={movies}/>
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
       </div>
     </>
-  );
+  )
 }
+
+export default Home;
