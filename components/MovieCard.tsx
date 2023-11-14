10cc9d109b03f8bfef100b56e474da483b26545b
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-11-14 11:28:04 +0530

    feat: add play button

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRouter } from "next/router";
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteButton from "./FavoriteButton";
 
@@ -7,6 +8,7 @@ interface MovieCardProps {
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
+  const router = useRouter();
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <img
@@ -88,7 +90,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
                         transition
                         hover:bg-neutral-300
                         "
-              onClick={() => {}}
+              onClick={() => router.push(`/watch/${data?.id}`)}
             >
               <BsFillPlayFill size={30} />
             </div>
