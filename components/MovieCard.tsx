808b4356698776e8a8ad9f2e417d8733995090a9
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2023-11-16 13:19:58 +0530

    feat: add movie modal info

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useRouter } from "next/router";
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteButton from "./FavoriteButton";
+import useInfoModalStore from "@/hooks/useInfoModal";
+import { BsChevronDown } from "react-icons/bs";
 
 interface MovieCardProps {
   data: Record<string, any>;
@@ -9,6 +11,8 @@ interface MovieCardProps {
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const router = useRouter();
+  const { openModal } = useInfoModalStore();
+
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <img
@@ -95,6 +99,9 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
               <BsFillPlayFill size={30} />
             </div>
             <FavoriteButton movieId={data.id} />
+            <div onClick={() => openModal(data?.id)} className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+              <BsChevronDown size={30} className="text-white group-hover/item:text-neutral-300 w-4" />
+            </div>
           </div>
 
           <p className="text-green-400 font-semibold mt-4">
